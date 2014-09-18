f7f9315eab06e69fe57d66bcb62f8b08674b0698
Jose Padilla <jpadilla1@users.noreply.github.com>
2014-09-17 21:31:15 -0400

    buffed up the raffle app

--- raffle.js
+++ raffle.js
@@ -1,34 +1,35 @@
-document.getElementById('start').onsubmit = function(){
-	var list = document.getElementsByTagName('li'), invalidNumbers = [], randomNumber;
-	var quantity = document.getElementById('quantity').value;
-	if(!isNaN(quantity)){
-		var ul = document.getElementById('list');
-		for(var a = 0; a < quantity; a++){
-			var li = document.createElement('li');
-			ul.appendChild(li);
-		}
-	}
-	else{
-		alert('Add a number in the field.');
-		return true;
-	}
-	for(var a = 0; a < list.length; a++){
+"use strict";
 
-		do{
-			randomNumber = getRandomInt(1, 30);
-		}while(invalidNumbers.indexOf(randomNumber) >= 0);
-		
-		invalidNumbers.push(randomNumber);
+document.getElementById('myForm').onsubmit = function(e) {
+    e.preventDefault();
+    var invalidNumbers = [],
+        randomNumber;
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
+    var quantity = document.getElementById('quantity').value;
+    if (!isNaN(quantity) && quantity <= (end-start)+1 && start < end) {
+        var ul = document.getElementById('list');
+        ul.innerHTML = "";
+        for (var a = 0; a < quantity; a++) {
+            var li = document.createElement('li');
+            li.className = "text-center winingNumber";
 
-		list[a].appendChild(document.createTextNode(randomNumber));
-	}
+            do {
+                randomNumber = getRandomInt(parseInt(start), parseInt(end));
+            } while (invalidNumbers.indexOf(randomNumber) >= 0);
 
-	return false;
+            invalidNumbers.push(randomNumber);
+
+            li.innerHTML = randomNumber;
+            ul.appendChild(li);
+        }
+    }
 };
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    var num = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (num.toString().length == 1) {
+        num = "0" + num.toString();
+    }
+    return num;
 }
-
-// function makeVisible(item){
-// }
